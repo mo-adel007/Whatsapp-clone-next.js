9295903c684d24cece7be5f00482414bdc30031e
mo-adel007 <67648966+mo-adel007@users.noreply.github.com>
2021-06-18 16:20:05 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCkPB2jHHOUUmWlASHyOrdUwSfobXkAdz8",
-  authDomain: "whatsapp-2-8b517.firebaseapp.com",
-  projectId: "whatsapp-2-8b517",
-  storageBucket: "whatsapp-2-8b517.appspot.com",
-  messagingSenderId: "795966024111",
-  appId: "1:795966024111:web:b52ba85317ef160e91675a",
-  measurementId: "G-22P3W0N438",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 const app = !firebase.apps.length
